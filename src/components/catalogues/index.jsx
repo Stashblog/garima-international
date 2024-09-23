1dd03bcb1e4a746d0fb79f4abaeabb6694295e27
Rishikarathore0601 <it2011x@global.org.in>
2024-09-23 14:55:20 +0530

    wip

--- src/components/catalogues/index.jsx
+++ src/components/catalogues/index.jsx
@@ -1,14 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import s from './styles.module.scss'
 
-const Catalogues = ({ imageSrc , title }) => {
+const Catalogues = ({ imageSrc, title, id }) => {
+  const navigate = useNavigate()
+
+  const handleViewCatalogue = () => {
+    navigate(`/catalogue/${id}`) // Redirect to the catalogue description page
+  }
+
   return (
     <div className={s.imageCard}>
-    <div className={s.imageWrapper}>
-      <img src={imageSrc} alt={title} className={s.image} />
+      <div className={s.imageWrapper}>
+        <img src={imageSrc} alt={title} className={s.image} />
+        <div className={s.overlay}>
+          <button className={s.viewButton} onClick={handleViewCatalogue}>
+            View Catalogue
+          </button>
+        </div>
+      </div>
+      <h3 className={s.title}>{title}</h3>
     </div>
-    <h3 className={s.title}>{title}</h3>
-  </div>
-  );
+  )
 }
 
-export default Catalogues;
+export default Catalogues
--- src/pages/demo/topCatalogues/index.jsx
+++ src/pages/demo/topCatalogues/index.jsx
@@ -1,14 +1,15 @@
+import React from 'react'
 import { Catalogues } from 'components'
 import s from './styles.module.scss'
 import images from 'images'
 
 const cataloguesData = [
-  { imageSrc: images.Cat, title: 'Home Decor' },
-  { imageSrc: images.Cat2, title: 'Showpiece Pairs' },
-  { imageSrc: images.Cat3, title: 'Antiques' },
-  { imageSrc: images.Cat, title: 'Home Decor' },
-  { imageSrc: images.Cat2, title: 'Showpiece Pairs' },
-  { imageSrc: images.Cat3, title: 'Antiques' }
+  { id: 1, imageSrc: images.Cat, title: 'Home Decor' },
+  { id: 2, imageSrc: images.Cat2, title: 'Showpiece Pairs' },
+  { id: 3, imageSrc: images.Cat3, title: 'Antiques' },
+  { id: 4, imageSrc: images.Cat, title: 'Home Decor' },
+  { id: 5, imageSrc: images.Cat2, title: 'Showpiece Pairs' },
+  { id: 6, imageSrc: images.Cat3, title: 'Antiques' }
 ]
 
 export default function Main() {
@@ -17,7 +18,12 @@ export default function Main() {
       <div className={s.title}>Top Catalogues</div>
       <div className={s.cardGrid}>
         {cataloguesData.map((catalogue, index) => (
-          <Catalogues key={index} imageSrc={catalogue.imageSrc} title={catalogue.title} />
+          <Catalogues
+            key={catalogue.id}
+            imageSrc={catalogue.imageSrc}
+            title={catalogue.title}
+            id={catalogue.id} // Pass the ID to the Catalogues component
+          />
         ))}
       </div>
     </div>
