2515f0f212641e2b7687f804bbbc3399a75a9754
Samarth kumar pal <palsamarth02@gmail.com>
2024-09-23 17:59:06 +0530

    Swiper added

--- src/pages/demo/homepage/index.jsx
+++ src/pages/demo/homepage/index.jsx
@@ -3,11 +3,52 @@ import s from './styles.module.scss'
 
 
 export default function Main() {
+
+  //   useEffect(() => {
+  //   if (images.length) {
+  //     const swiper = new window.Swiper(`.swiper`, {
+  //       slidesPerView: 1,
+  //       spaceBetween: 50,
+  //       loop: true,
+  //       autoplay: {
+  //         delay: 5000, // Adjust as needed
+  //         disableOnInteraction: false
+  //       },
+  //       pagination: {
+  //         el: '.swiper-pagination',
+  //         clickable: true,
+  //         dynamicBullets: true
+  //       },
+  //       navigation: {
+  //         nextEl: '.swiper-button-next',
+  //         prevEl: '.swiper-button-prev'
+  //       }
+  //     })
+
+  //     return () => {
+  //       swiper.destroy(true, true)
+  //     }
+  //   }
+  // }, [images])
+  
   return (
     <>
     <div className={s.main}>
     <div className={s.dashboard}>
       <div className={s.content + ' indent'}>
+{/* 
+         <div className={'swiper topBanners ' + s.swiperContainer}>
+            <div className='swiper-wrapper'>
+              {images.map((img, i) => (
+                <div className={s.slide + ' swiper-slide'} key={i}>
+                  <ImageTag src={img} alt='' />
+                </div>
+              ))}
+            </div>
+            <div className='swiper-button-next'></div>
+            <div className='swiper-button-prev'></div>
+            <div className='swiper-pagination'></div>
+          </div> */}
         <div className="front">
           <Card
             title='Garima International'
